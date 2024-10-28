d3c2680854f7ffbdb36724e2d963c3c409def9a9
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-28 22:15:26 +0200

    Completed Task 2

--- func.py
+++ func.py
@@ -1,5 +1,17 @@
 def main():
-    try:
-        pass
-    except Exception as e:
-        print(f'Error: {e}')
+    numbers, letters = counter(str(input('Enter the text: ')))
+    print(f'Amount of letters: {letters}\nAmount of numbers: {numbers}')
+
+def counter(text):
+    amount_of_numbers = 0
+    amount_of_letters = 0
+
+    pattern_of_symbols = r" [',.:/\\!@#$%^&*()_+{}\[\];\"<>?|`~\-]"
+
+    for i in text:
+        if '9' >= i >= '0':
+            amount_of_numbers += 1
+        elif i not in pattern_of_symbols:
+            amount_of_letters += 1
+
+    return amount_of_numbers, amount_of_letters
