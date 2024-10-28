5ae03e2c262db1c1c247cdbc6a513a264dfe7072
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-28 21:42:55 +0200

    Completed Task 1

--- func.py
+++ func.py
@@ -1,5 +1,19 @@
 def main():
-    try:
-        pass
-    except Exception as e:
-        print(f'Error: {e}')
+    print('Inverted text:', inverter(str(input('Enter the text: '))))
+
+def _len(data):
+    number = 0
+
+    for temp in data:
+        number += 1
+
+    return number
+
+def inverter(text):
+    length = _len(text)
+    result = str()
+
+    for i in range(length):
+        result += str(text[length - i - 1])
+
+    return result
