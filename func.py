1179596f828d9f3e02e837ab5bb7a6fdbd339933
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-28 22:23:42 +0200

    Started

--- func.py
+++ func.py
@@ -1,8 +1,14 @@
 def main():
     try:
-        print(finder(input('Enter the text: '), input('Enter the character you want to search for: ')))
+        print('The number of times the symbol is repeated:', finder(input('Enter the text: '), input('Enter the character you want to search for: ')))
     except Exception as e:
         print(f'Error: {e}')
 
 def finder(text, char):
-    pass
+    result = 0
+
+    for i in text:
+        if i == char:
+            result += 1
+
+    return result
