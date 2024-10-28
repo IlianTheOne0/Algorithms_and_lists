0c6e5836ff22e6db0458e6875f4602f1535d4ea0
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-28 22:37:45 +0200

    Started

--- func.py
+++ func.py
@@ -1,5 +1,15 @@
 def main():
     try:
-        pass
+        # print('The number of times the word is repeated:', finder(input('Enter the text: '), input('Enter the word you want to search for: ')))
+        print(finder('Hello world hello world world hello World hello Wello Horld', 'world'))
     except Exception as e:
         print(f'Error: {e}')
+
+def finder(text, word):
+    result = 0
+
+    for temp in text:
+        if word == temp:
+            result += 1
+
+    return result
