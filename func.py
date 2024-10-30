3d8c0e1e8b372585e39c6c9d12c67cb9d8f38b4f
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-31 01:18:07 +0200

    Completed Task 1

--- func.py
+++ func.py
@@ -1,5 +1,19 @@
 def main():
     try:
-        pass
+        print('Result:', calc(*input('Enter the math expression, separated with space: ').split(' ')))
     except Exception as e:
         print(f'Error: {e}')
+
+def calc(first_number, operator, second_number) -> int:
+    first_number, second_number = int(first_number), int(second_number)
+
+    if operator == '+':
+        return first_number + second_number
+    elif operator == '-':
+        return first_number - second_number
+    elif operator == '*':
+        return first_number * second_number
+    elif operator == '/':
+        return first_number / second_number
+    else:
+        raise ValueError(f'unsupported operator {operator}')
