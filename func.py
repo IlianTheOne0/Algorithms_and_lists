b9d8a5592654e5aec46cdd7370ec2cb946445f89
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-31 00:20:33 +0200

    Completed Task 4

--- func.py
+++ func.py
@@ -1,14 +1,19 @@
 def main():
-    try:
-        # print('The number of times the word is repeated:', finder(input('Enter the text: '), input('Enter the word you want to search for: ')))
-        print(finder('Hello world hello world world hello World hello Wello Horld', 'world'))
-    except Exception as e:
-        print(f'Error: {e}')
+    print('The number of times the word is repeated:', finder(input('Enter the text: '), input('Enter the word you want to search for: ')))
 
-# Function to find the number of times a word is repeated in a text
+def spliter(text) -> list:
+    pattern = '.,!?@#$%^&*()_+'
 
-def replacer() -> str:
-    pass
+    for char in pattern:
+        text = text.replace(char, '')
+
+    return text.lower().split()
 
 def finder(text, word) -> int:
-    return text.lower().split().count(word.lower())
+    result = 0
+
+    for i in spliter(text):
+        if i == word.lower():
+            result += 1
+
+    return result
