e54a39a0bc2beb27170e5639c0c75f7031293a64
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-31 00:03:25 +0200

    Edited Task 3

--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 def main():
     print('The number of times the symbol is repeated:', finder(input('Enter the text: '), input('Enter the character you want to search for: ')))
 
-def finder(text, char):
+def finder(text, char) -> int:
     result = 0
 
     for i in text:
