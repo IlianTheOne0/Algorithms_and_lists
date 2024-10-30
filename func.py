47a59b63d3f45f9caad4afd1b4796c86807de031
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-31 00:57:27 +0200

    Completed Task 5

--- func.py
+++ func.py
@@ -1,5 +1,17 @@
 def main():
-    try:
-        pass
-    except Exception as e:
-        print(f'Error: {e}')
+    print('Your restructured text:', replacer(input('Enter the text: '), input('Enter the word you want to replace: '), input('Enter the word you want to replace with: ')))
+
+def spliter(text) -> list:
+    pattern = '.,!?@#$%^&*()_+'
+
+    for char in pattern:
+        text = text.replace(char, '')
+
+    return text.lower().split()
+
+def replacer(text, to_replace, how_to_replace) -> str:
+    for i in spliter(text):
+        if i == to_replace.lower():
+            text = text.replace(i, how_to_replace)
+
+    return text
